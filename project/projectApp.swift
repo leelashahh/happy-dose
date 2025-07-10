29b9f59083792e0a745b7b861043f5b38d97eb73
leelashahh <leela0129@icloud.com>
2025-07-10 12:17:40 -0500

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by DPI Student 145 on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ projectTests/projectTests.swift
@@ -0,0 +1,16 @@
+//
+//  projectTests.swift
+//  projectTests
+//
+//  Created by DPI Student 145 on 7/10/25.
+//
+
+import Testing
+
+struct projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
